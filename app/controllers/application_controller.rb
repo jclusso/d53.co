a0fbed3a9ec921779bedf72649e92577dbfb30d1
Jarrett Lusso <jclusso@gmail.com>
2024-02-12 20:41:59 -0500

    Remove browser restrictions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
   default_form_builder ApplicationFormBuilder
 end
