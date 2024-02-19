aa6d5f6709e79775b4b0d4ba384d1a9e104e0b40
Jarrett Lusso <jclusso@gmail.com>
2024-02-19 15:17:26 -0500

    Remove random test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :other_items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
