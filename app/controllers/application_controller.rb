1fafe575bf7f1efffe81e3960491eff3fd76f154
Jarrett Lusso <jclusso@gmail.com>
2024-02-25 13:04:43 -0500

    Restrict redirect routes
    The redirect route was allowing too much through. Removed ability to query by domain without a valid type since it's prone to many unintentional requests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   default_form_builder ApplicationFormBuilder
+
+  private
+
+  def not_found
+    raise ActionController::RoutingError.new("Not Found")
+  end
 end
--- test/controllers/redirect_controller_test.rb
+++ test/controllers/redirect_controller_test.rb
@@ -23,13 +23,29 @@ class RedirectControllerTest < ActionDispatch::IntegrationTest
     assert_equal @domain, new_query.domain
   end
 
-  test "should create query with domain" do
+  test "should 404 unless type and domain" do
     get "/#{@domain}"
+    assert_response :not_found
+  end
 
-    assert_redirected_to query_path(new_query)
-    assert_equal 'Cloudflare', new_query.server
-    assert_equal 'A', new_query.type
-    assert_equal @domain, new_query.domain
+  test "should 404 unless valid type" do
+    get "/x/#{@domain}"
+    assert_response :not_found
+  end
+
+  test "should 404 unless server" do
+    get "/flarecloud/a/#{@domain}"
+    assert_response :not_found
+  end
+
+  test "should allow case insensitive type" do
+    get "/A/#{@domain}"
+    assert_response :redirect
+  end
+
+  test "should allow case insensitive server" do
+    get "/GoOgLe/a/#{@domain}"
+    assert_response :redirect
   end
 
   private
